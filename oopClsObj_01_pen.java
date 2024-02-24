30b7ad5ddcd7551127292646d47162134c2296fc
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-24 22:04:02 +0600

    This is very basic class and object in oops in java.

---
+++ oopClsObj_01_pen.java
@@ -0,0 +1,13 @@
+class Pen {
+    public void write() {
+        System.out.println("Writing can be done py pen.");
+    }
+}
+
+public class oopClsObj_01_pen {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen(); 
+
+        pen1.write();
+    }
+}
